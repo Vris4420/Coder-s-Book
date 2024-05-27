ff0fde03e627cc891f188f6603cb210d0b8e834f
Vrishank Sharma <sharma08462@gmail.com>
2024-05-27 12:11:32 +0530

    add fixes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 
 import Image from "next/image";
 import Banner from "./components/Banner";
+import ProductList from "./components/ProductList";
 
 export default function Home() {
   return (
    <>
    <Banner/>
+   <ProductList/>
    </>
   );
 }
